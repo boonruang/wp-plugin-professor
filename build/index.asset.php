70fb42e28853a8d2562cbfd9252ea71d118d3678
boonruang <scubatoy@gmail.com>
2022-08-26 04:34:31 +0700

    151. Control Post Meta With Block Type

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-data', 'wp-element'), 'version' => '3894060864fad5b2fd1d');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-data', 'wp-element'), 'version' => '038ec21728b7716d912c');
